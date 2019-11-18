7801fe2fac924dd95185847d707ab4f5bc4849b7
Jim <303683086@qq.com>
2019-11-18 16:50:39 +0800

    fixed stuff

--- src/rfc822/rfc822.hpp
+++ src/rfc822/rfc822.hpp
@@ -1,52 +1,85 @@
 /**-----------------------------------------------------------------------------------------------------------------
  * @file	rfc822.hpp
- * @brief	packetize and depacketize HTTP message
+ * @brief	Standrad for ARPA Ineternet text messages	
+ * @ref		IETF-RFC822
  *
  * Copyright (c) 2019-2019 Jim Zhang 303683086@qq.com
  *------------------------------------------------------------------------------------------------------------------
 */
 
 
-#ifndef __LIBMIME_MESSAGE_HPP__
-#define __LIBMIME_MESSAGE_HPP__
+#ifndef __LIBMIME_RFC822_HPP__
+#define __LIBMIME_RFC822_HPP__
 
 
 /*------------------------------------------------------------------------------------------------------------------
  *
- *										    MESSAGE INCLUDES
+ *												RFC822 INCLUDES
  *
  *------------------------------------------------------------------------------------------------------------------
 */
 
-#include <libMIME/rfc822/rfc822.hpp>
+#include <libMIME/src/rfc822/rfc822.hpp>
 
 
 namespace NS_LIBMIME{
 
 /*------------------------------------------------------------------------------------------------------------------
  *
- *										    MESSAGE DATA BLOCK
+ *												RFC822 DATA BLOCK
  *
  *------------------------------------------------------------------------------------------------------------------
 */
 
 /**
- *	@brief HTTP official request method
+ *	@brief RFC822 (Header) Field class and function set
  **/
-
-/**
- *	@brief HTTP message class and function set
- **/
-class message : public mime{
+class rfc822_field{
 	public:
+		void set();
 
-	private:
-		string message_line;
+	protected:
+		string field_name;
+		string field_body;
 };
+
+#if 0
+1. header CRLF 
+   CRLF
+   body
+
+2. body fold and unfold
+	fold : insert Y only if X exist
+
+	X = 1*([CRLF] + SPACE/HTAB)	;	SPACE = ASCII_32, HTAB = ASCII_9
+	Y = CRLF + 1*(SPACE/HTAB)
+
+	unfold : Y -> SPACE/HTAB 
+
+3. filed  
+
+   filed-name:field-body+CRLF
+
+   filed-name Consist of ASCII 33~126 exclude ":"
+
+   field-body Consist of ANY ASCII exclude CRLF
+
+   Note: An unfold action SHOULD BE done, before parse field-body
+
+   Note: field body can be ignored
+
+#endif
+
+#if 0 //TBD
+class message{};
+class dateTime{};
+class address{};
+more for structrued field-body details
+#endif
 
 
 } /* namespace NS_LIBMIME */
 
 
-#endif /*__LIBMIME_MESSAGE_HPP__*/
+#endif /*__LIBMIME_RFC822_HPP__*/
 
